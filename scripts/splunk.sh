a4bf9bbf96f4670dba219d37ec780536806313d2
Jeremy Keen <jeremykeen@gmail.com>
2016-03-30 10:12:27 -0400

    ScaleIO SDC installation

--- scripts/splunk.sh
+++ scripts/splunk.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+#install scaleio pre-reqs
+yum install numactl libaio -y
+
+#install ScaleIO SDC
+cd /vagrant/scaleio/ScaleIO_2.0.0_RHEL6_Download
+MDM_IP=192.168.50.12,192.168.50.13 rpm -Uv EMC-ScaleIO-sdc-2.0-5014.0.el6.x86_64.rpm
+
+#install splunk
 cd /vagrant
 tar xvzf splunk-6.3.3-f44afce176d0-Linux-x86_64.tgz -C /opt
 sudo /opt/splunk/bin/splunk start --accept-license
